b29ab5cea0bd08bc39995fd81652639dd599cc21
supurazako <redhot30atama@gmail.com>
2023-04-16 21:38:05 +0900

    upload to github

---
+++ src/main/java/io/github/supurazako/tpplayer/TPPlayer.java
@@ -0,0 +1,87 @@
+package io.github.supurazako.tpplayer;
+
+import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+public final class TPPlayer extends JavaPlugin {
+
+
+    private static final String CONFIG_FILE_NAME = "config.yml";
+    private FileConfiguration config;
+
+
+    @Override
+    public void onEnable() {
+        // プラグインが有効になったときに実行される処理
+        // コマンドの登録
+        PluginCommand tpplayerCommand = getCommand("tpplayer");
+        if (tpplayerCommand != null) {
+            tpplayerCommand.setExecutor(new TPPlayerCommand());
+        } else {
+            getLogger().warning("Failed to register TPPlayer command. Plugin command is null");
+        }
+        loadConfig();
+    }
+
+    @Override
+    public void onDisable() {
+        // プラグインが無効になったときに実行される処理
+        saveConfig();
+    }
+
+
+    // コンフィグファイルの読み込みメソッド
+    private void loadConfig() {
+        // config.yml ファイルのパスを取得
+        File configFile = new File(getDataFolder(), CONFIG_FILE_NAME);
+
+
+        // config フィールドが null の場合に初期化
+        if (config == null) {
+            config = new YamlConfiguration();
+        }
+
+
+        // config.yml ファイルが存在するか確認し、存在したら読み込む
+        if (configFile.exists()) {
+            config = YamlConfiguration.loadConfiguration(configFile);
+            getLogger().info("Loaded config.yml.");
+        } else {
+            getLogger().info("config.yml does not exist. Generating default configuration.");
+            // config フィールドにデフォルトの設定を生成し、保存する
+            config = new YamlConfiguration();
+            config.set("permissions.tpplayer\\.tp.description", "Allows the use of tpplayer command");
+            config.set("permissions.tpplayer\\.tp.default", true);
+
+
+            try {
+                config.save(configFile);
+                getLogger().info("saved config.yml.");
+            } catch (IOException e) {
+                getLogger().warning("Failed to save config.yml.");
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    // コンフィグファイルの保存を行うメソッド
+    public void saveConfig() {
+        // config.yml ファイルのパスを取得
+        File configFile = new File(getDataFolder(), CONFIG_FILE_NAME);
+
+
+        // コンフィグファイルの保存を実行
+        try {
+            config.save(configFile);
+            getLogger().info("saved config.yml.");
+        } catch (IOException e) {
+            getLogger().warning("Failed to save config.yml.");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/io/github/supurazako/tpplayer/TPPlayerCommand.java
@@ -0,0 +1,136 @@
+package io.github.supurazako.tpplayer;
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+
+import java.util.ResourceBundle;
+
+
+public class TPPlayerCommand implements CommandExecutor {
+
+
+    private TPPlayer tpPlayer;
+
+
+    public void setTpPlayer(TPPlayer tpPlayer) {
+        this.tpPlayer = tpPlayer;
+    }
+
+
+    private static final String RESOURCE_BUNDLE_NAME = "messages";
+
+
+    private boolean isValidPlayerName(String playerName) {
+        // プレイヤー名がnullまたは空文字列の場合は無効
+        if (playerName == null || playerName.isEmpty()) {
+            return false;
+        }
+        // プレイヤー名が存在するかチェック
+        Player player = Bukkit.getPlayerExact(playerName);
+        return player != null && player.isValid();
+    }
+
+
+    //ユーザーの言語を取得するメソッド
+    private String getLanguage(CommandSender sender) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            String locale = player.locale().toString();
+            if (locale.contains("_")) {
+                locale = locale.split("_")[0]; // ロケールから言語コードのみを取得
+                return locale;
+            } else {
+                // ロケールがnullまたは不正な形式の場合、デフォルト言語を返す
+                return "en";
+            }
+        } else {
+            // Player以外の場合はデフォルト言語を返す
+            return "en";
+        }
+    }
+
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        //コマンドの実行時に実行される処理
+
+        try {
+            ResourceBundle bundle;
+            String language = getLanguage(sender); //ユーザーの言語を取得するメソッドの呼び出し
+            if (language.equalsIgnoreCase(("ja"))) {
+                bundle = ResourceBundle.getBundle(RESOURCE_BUNDLE_NAME + "_ja");
+            } else {
+                bundle = ResourceBundle.getBundle(RESOURCE_BUNDLE_NAME + "_en");
+            }
+
+
+            FileConfiguration config = tpPlayer.getConfig();
+
+
+            //エラーメッセージの取得
+            String onlyPlayersCommand = bundle.getString("ONLY_PLAYERS_COMMAND");
+            String invalidPlayerName = bundle.getString("INVALID_PLAYER_NAME");
+            String noPermission = bundle.getString("NO_PERMISSION");
+            String usage = bundle.getString("USAGE");
+            String offline = bundle.getString("OFFLINE");
+
+
+            //コマンドを実行したのがプレイヤーでない場合は処理を終了する
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(onlyPlayersCommand);
+                return true;
+            }
+
+            Player player = (Player) sender; //コマンドを実行したプレイヤーを取得
+
+            //権限をチェック
+            if(config.getBoolean("tpplayer.tp")) {
+                if (!sender.hasPermission("tpplayer.tp")) {
+                    sender.sendMessage(noPermission);
+                    return true;
+                }
+            }
+
+            //引数が足りない場合は使い方を表示する
+            if (args.length < 1) {
+                sender.sendMessage(usage);
+                return true;
+            }
+
+            String targetPlayerName = args[0];
+
+            //プレイヤー名が正規表現かチェック
+            if (!isValidPlayerName(targetPlayerName)) {
+                sender.sendMessage(invalidPlayerName);
+                return true;
+            }
+
+
+            Player targetPlayer = player.getServer().getPlayer(targetPlayerName); //テレポート先のプレイヤーを取得
+
+            //テレポート先のプレイヤーがオンラインかチェック
+            if (targetPlayer == null) {
+                sender.sendMessage(offline);
+                return true;
+            }
+
+            //テレポートを実行
+            player.teleport(targetPlayer);
+
+            sender.sendMessage("Teleported to " + targetPlayer.getName() + ".");
+            return true;
+        } catch (Exception e) {
+            // 例外が発生した場合の処理
+            sender.sendMessage("An error occurred while executing the command: " + e.getMessage());
+            e.printStackTrace(); // エラーのスタックトレースを出力
+            return false;
+        }
+    }
+}
