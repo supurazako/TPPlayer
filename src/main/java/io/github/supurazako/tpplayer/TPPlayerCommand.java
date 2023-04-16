a37a752bbc3c3f0c9fb6772e2c989c33f1ab7097
supurazako <redhot30atama@gmail.com>
2023-04-16 22:28:06 +0900

    テスト

--- src/main/java/io/github/supurazako/tpplayer/TPPlayerCommand.java
+++ src/main/java/io/github/supurazako/tpplayer/TPPlayerCommand.java
@@ -23,7 +23,7 @@ public class TPPlayerCommand implements CommandExecutor {
     }
 
 
-    private static final String RESOURCE_BUNDLE_NAME = "main.resources.message";
+    private static final String RESOURCE_BUNDLE_NAME = "main/resources./message";
 
 
     private boolean isValidPlayerName(String playerName) {
